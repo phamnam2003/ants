ba43bbd4fd776f180b44d9d89919ab4f59135f3f
phamnam2003 <namphamhai7@gmail.com>
2026-01-03 23:16:28 +0700

    chore(space): remove spacing

--- pkg/syncx/spinlock_test.go
+++ pkg/syncx/spinlock_test.go
@@ -1,7 +1,6 @@
 // Copyright 2021 Andy Pan & Dietoad. All rights reserved.
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
-
 package syncx
 
 import (
